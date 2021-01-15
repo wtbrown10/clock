26aa49b88d97537aaf2f86bf93e1c30369661486
Will brown <wtbrown10@gmail.com>
2021-01-14 20:26:38 -0500

    fixed version 3&5

--- src/com/will/Main.java
+++ src/com/will/Main.java
@@ -13,13 +13,13 @@ public class Main {
         System.out.println("version 2: ");
         hours.convertHours(21, 00, -5);
         System.out.println("version 3: ");
-        hours.convertMinutes(21, 00, 147);
+        hours.convertMinutes(21, 50, 147);
         System.out.println("version 4: ");
         hours.convertHours(21, 27, 2, false);
         hours.convertMinutes(21, 0, 147, true);
 
         System.out.println("version 5: ");
-        hours.convertHours(9, 27, "PM", 12, true);
+        hours.convertHours(9, 27, "am", 12, true);
         hours.convertMinutes(9,12,"pm", 147, true);
 
 
--- src/com/will/Utility.java
+++ src/com/will/Utility.java
@@ -1,19 +1,6 @@
 package com.will;
 
 public class Utility {
-//    private int hours;
-//    private int minutes;
-//    private int hoursPassed;
-//    private int minutesPassed;
-//    private boolean standard;
-
-   public Utility() {
-//        this.hours = hours;
-//        this.minutes = minutes;
-//        this.hoursPassed = hoursPassed;
-//        this.minutesPassed = minutesPassed;
-//        this.standard = standard;
-    }
 
     public void convertHours(int hours, int minutes, int hoursPassed) {
         int h = (hours + hoursPassed) % 24;
@@ -22,13 +9,13 @@ public class Utility {
     }
 
     public void convertMinutes(int hours, int minutes, int minutesPassed) {
-        int h = ((hours + (minutesPassed / 60)) % 24);
+        int h = ((hours + ((minutes + minutesPassed) / 60)) % 24);
         int m = ((minutes + minutesPassed) % 60);
         System.out.println(h + ":" + m);
 
     }
 
-   public void convertHours(int hours, int minutes, int hoursPassed, boolean standard) {
+   public void convertHours(int hours, int minutes, int hoursPassed , boolean standard) {
        if (standard) {
            int h = (hours + hoursPassed) % 12;
            int m = (minutes) % 60;
@@ -43,7 +30,7 @@ public class Utility {
 
     public void convertMinutes(int hours, int minutes, int minutesPassed, boolean standard) {
        if (standard) {
-           int h = ((hours + (minutesPassed / 60)) % 12);
+           int h = ((hours + ((minutes + minutesPassed)/ 60)) % 12);
            int m = ((minutes + minutesPassed) % 60);
            System.out.println("Standard Time: " + h + ":" + m );
            return;
@@ -53,14 +40,14 @@ public class Utility {
             System.out.println("Military Time: " + h + ":" + m);
     }
 
-    public String morningOrNight(int num) {
+    private String amOrPm1(int num) {
        if  (((num/12) % 2) < 1) {
             return "am";
         }
        return "pm";
     }
 
-    public String morningOrNight2(int num) {
+    private String amOrPm2(int num) {
         if  ((num % 2) < 1) {
             return "am";
         }
@@ -69,31 +56,38 @@ public class Utility {
 
 
     public void convertHours(int hours, int minutes, String suffix, int hoursPassed, boolean standard) {
-       if (standard) {
-           int h = (hours + hoursPassed) % 12;
+        if (standard) {
+            int h = (hours + hoursPassed) % 12;
+            int m = (minutes) % 60;
+
+            /*((hours + hoursPassed/12) % 2) < 1*/
+            System.out.println("Standard Time: " + h + ":" + m + amOrPm1(h));
+
+            return;
+        } else if (suffix == "pm"){
+            int h = (hours + 12 + hoursPassed) % 24;
+            int m = (minutes) % 60;
+            System.out.println("Military Time: " + h + ":" + m);
+       } else if (suffix == "am"){
+           int h = (hours + hoursPassed) % 24;
            int m = (minutes) % 60;
-
-           /*((hours + hoursPassed/12) % 2) < 1*/
-           System.out.println("Standard Time: " + h + ":" + m + morningOrNight(h));
-
-           return;
+           System.out.println("Military Time: " + h + ":" + m);
        }
-        int h = (hours + hoursPassed) % 24;
-        int m = (minutes) % 60;
-        System.out.println("Military Time: " + h + ":" + m);
+
     }
 
     public void convertMinutes(int hours, int minutes, String suffix, int minutesPassed, boolean standard) {
         if (standard) {
-            int h = ((hours + (minutesPassed / 60)) % 12);
-            int h1 = (hours + (minutesPassed / 60));
+            int h = ((hours + ((minutes + minutesPassed) / 60)) % 12);
+            int h1 = (hours + ((minutes + minutesPassed) / 60));
             int m = ((minutes + minutesPassed) % 60);
-            System.out.println("Standard Time: " + h + ":" + m + morningOrNight2(h1) );
+            System.out.println("Standard Time: " + h + ":" + m + amOrPm2(h1) );
             return;
         }
-        int h = ((hours + (minutesPassed / 60)) % 24);
+        int h = (((hours + 12) + ((minutes + minutesPassed) / 60)) % 24);
         int m = ((minutes + minutesPassed) % 60);
         System.out.println("Military Time: " + h + ":" + m);
+
     }
 
 }
